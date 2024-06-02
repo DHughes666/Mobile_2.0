99cc88d0dc15726c829d51b82fd6cf66fe325516
DHughes666 <graziemolto6@gmail.com>
2024-06-02 05:23:14 +0100

    Ballsy ...Up and running

--- fun_app/App.js
+++ fun_app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ball from '../fun_app/src/newe/ball';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Worldy!</Text>
+      <Ball />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
